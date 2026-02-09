c4444b98ab3dff686abce53b602620b25a9d9e4d
Timofii <regen69neger@gmail.com>
2026-02-09 12:05:53 +0200

    implemented logic into storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,48 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private K[] keyArray = (K[]) new Object[10];
+    private V[] valueArray = (V[]) new Object[10];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < keyArray.length; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                valueArray[i] = value;
+                return;
+            }
+        }
+        for (int i = 0; i < keyArray.length; i++) {
+            if (keyArray[i] == null && valueArray[i] == null) {
+                keyArray[i] = key;
+                valueArray[i] = value;
+                return;
+            }
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < keyArray.length; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        int counter = 0;
+        for (int i = 0; i < keyArray.length; i++) {
+            if (valueArray[i] != null) {
+                counter++;
+            }
+        }
+        return counter;
     }
+
 }
